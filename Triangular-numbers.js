79135946738cb1e6e24ee6de54920bc48cdbe514
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-05-30 12:54:45 -0700

    Create Triangular-numbers.js

---
+++ Triangular-numbers.js
@@ -0,0 +1,15 @@
+/*
+Triangular numbers are so called because of the equilateral 
+triangular shape that they occupy when laid out as dots.
+
+You need to return the nth triangular number. 
+You should return 0 for out of range values:
+
+Example:
+  0 --> 0
+  2 --> 3
+  3 --> 6
+  -10 --> 0
+*/
+
+
